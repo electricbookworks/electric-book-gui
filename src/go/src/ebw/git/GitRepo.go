5821f9c2d949aa8ff4ab797d2351475208c7a8de
Craig Mason-Jones <craig@lateral.co.za>
2017-04-18 09:39:49 +0200

    Merged

---
+++ src/go/src/ebw/git/GitRepo.go
@@ -0,0 +1,47 @@
+package git
+
+import (
+	// "github.com/golang/glog"
+	"github.com/google/go-github/github"
+
+	"ebw/util"
+)
+
+type GitRepo struct {
+	*github.Repository
+
+	lastCommit *CommitInfo
+}
+
+func (gr *GitRepo) GetLastCommit() *CommitInfo {
+	gr.lastCommit.waiting.Wait()
+	return gr.lastCommit
+}
+
+// FetchRepos fetches all the repositories for the client
+func FetchRepos(client *Client) ([]*GitRepo, error) {
+	repos, _, err := client.Repositories.List(client.Context, "",
+		&github.RepositoryListOptions{
+			ListOptions: github.ListOptions{
+				PerPage: 500,
+				Page:    1,
+			},
+			Direction:  `asc`,
+			Sort:       `name`,
+			Visibility: `all`,
+		})
+
+	if nil != err {
+		return nil, util.Error(err)
+	}
+	grs := make([]*GitRepo, len(repos))
+	for i, r := range repos {
+		gr := &GitRepo{Repository: r}
+		lc, err := LastCommit(client, gr.GetName())
+		if nil == err {
+			gr.lastCommit = lc
+		}
+		grs[i] = gr
+	}
+	return grs, nil
+}
