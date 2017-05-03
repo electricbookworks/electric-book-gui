b1d409b5b8761b0b9795ee7ccbd3d52e032cf774
katmutua <jkm.mutua@gmail.com>
2017-05-03 19:17:33 +0300

    Modify repo diffs function

--- src/go/src/ebw/git/GitRepoDiff.go
+++ src/go/src/ebw/git/GitRepoDiff.go
@@ -4,28 +4,13 @@ import (
 	"github.com/google/go-github/github"
 	"github.com/golang/glog"
 )
-
-
-//given two branches return the comparison
-
+//compare two commits given a certain repo
 func RepoDiffs(client *Client, repoName string, brA string, brB string) (*github.CommitsComparison, error) {
-	comparison, _, err := client.Repositories.CompareCommits(client.Context, client.Username,
+	c, _, err := client.Repositories.CompareCommits(client.Context, client.Username,
 		repoName, brA, brB)
 
 	if nil != err {
 		glog.Errorf(`Error on repo diffs(%s): %s`, repoName, err.Error())
-		return err
 	}
-
-	return comparison, nil
-}
-
-func GetAllBranches(client *Client, repoName string) ([]*github.Branch, error) {
-	branchList, _, err := client.Repositories.ListBranches(client.Context,
-		client.Username, repoName, nil)
-
-	if nil != err {
-		glog.Errorf(`Error on getallbranches(%s): %s`, repoName, err.Error())
-	}
-	return branchList, nil
+	return c, nil
 }
