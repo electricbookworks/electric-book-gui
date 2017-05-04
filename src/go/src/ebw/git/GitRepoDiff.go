a76783e67c05e644fac2b7c00086e3eba3463d17
katmutua <jkm.mutua@gmail.com>
2017-05-04 11:17:40 +0300

    Add ahead and behind info to a type

--- src/go/src/ebw/git/GitRepoDiff.go
+++ src/go/src/ebw/git/GitRepoDiff.go
@@ -1,16 +1,24 @@
 package git
 
 import (
-	"github.com/google/go-github/github"
 	"github.com/golang/glog"
 )
+
+type RepoDiffStats struct {
+	Ahead  int
+	Behind int
+}
 //compare two commits given a certain repo
-func RepoDiffs(client *Client, repoName string, brA string, brB string) (*github.CommitsComparison, error) {
+func RepoDiffs(client *Client, repoName string, brA string, brB string) (RepoDiffStats, error) {
+	cs := RepoDiffStats{}
 	c, _, err := client.Repositories.CompareCommits(client.Context, client.Username,
 		repoName, brA, brB)
 
 	if nil != err {
 		glog.Errorf(`Error on repo diffs(%s): %s`, repoName, err.Error())
 	}
-	return c, nil
+	cs.Ahead = c.GetAheadBy()
+	cs.Behind = c.GetBehindBy()
+
+	return cs, nil
 }
