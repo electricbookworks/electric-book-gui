8de7f5a86b0ea646fb032c896e6c71351886f5d9
katmutua <jkm.mutua@gmail.com>
2017-05-09 15:46:42 +0300

    Modify compare commit func

--- src/go/src/ebw/git/GitRepoDiff.go
+++ src/go/src/ebw/git/GitRepoDiff.go
@@ -2,18 +2,22 @@ package git
 
 import (
 	"github.com/golang/glog"
+	"fmt"
 )
 
 type RepoDiffStats struct {
 	Ahead  int
 	Behind int
 }
-//compare two commits given a certain repo
-func RepoDiffs(client *Client, repoName string, brA string, brB string) (RepoDiffStats, error) {
+func CompareCommits(client *Client, repoName string, repoOwnerA string,
+	repoBranchA string, repoOwnerB string, repoBranchB string) (RepoDiffStats, error) {
 	cs := RepoDiffStats{}
-	c, _, err := client.Repositories.CompareCommits(client.Context, client.Username,
-		repoName, brA, brB)
 
+	base := fmt.Sprintf("%s:%v", repoOwnerA, repoBranchA)
+	head := fmt.Sprintf("%s:%v", repoOwnerB, repoBranchB)
+
+	c, _, err := client.Repositories.CompareCommits(client.Context, client.Username,
+		repoName, base, head)
 	if nil != err {
 		glog.Errorf(`Error on repo diffs(%s): %s`, repoName, err.Error())
 	}
