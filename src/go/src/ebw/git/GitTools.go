f492ff09ba5f2b837b2a5cf11aa2297a851f2bf5
Craig Mason-Jones <craig@lateral.co.za>
2017-11-16 17:09:09 +0200

    Flash message on invitation

--- src/go/src/ebw/git/GitTools.go
+++ src/go/src/ebw/git/GitTools.go
@@ -369,7 +369,7 @@ func DuplicateRepo(client *Client, githubPassword string,
 
 // ContributeToRepo configures a fork of the repo for the given
 // user, and checks out the newly forked repo.
-func ContributeToRepo(client *Client, repoUserAndName string, private bool) error {
+func ContributeToRepo(client *Client, repoUserAndName string, private bool, msgCallback func(msg string)) error {
 	glog.Infof(`client.Username=%s, ContributeToRepo: %s`, client.Username, repoUserAndName)
 	// See CLI BookContribute for model of how this should function.
 	parts := strings.Split(repoUserAndName, `/`)
@@ -398,6 +398,7 @@ func ContributeToRepo(client *Client, repoUserAndName string, private bool) erro
 		}
 		// If the fork is PRIVATE, we need to grant access AT LEAST READ ACCESS to the
 		// upstream repo owner - repoUser - so that upstream can see PR's.
+		glog.Infof("Inviting %s to contribute to %s/%s", repoUser, client.Username, repoName)
 		if _, err := client.Repositories.AddCollaborator(
 			client.Context,
 			client.Username, repoName,
@@ -407,6 +408,7 @@ func ContributeToRepo(client *Client, repoUserAndName string, private bool) erro
 			}); nil != err {
 			return util.Error(err)
 		}
+		msgCallback(fmt.Sprintf("%s has been invited to contribute to your repo %s/%s", repoUser, client.Username, repoName))
 	}
 
 	repoDir, err := RepoDir(client.Username, client.Username, repoName)
